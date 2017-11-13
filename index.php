3d381949d3afb0a0d97343778831877a96ca8068
Daniel Heron <daniel@superheron.com>
2017-11-12 19:46:03 -0700

    adds initial php files

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php
+  include('includes/header.php');
+  ?>
+<!-- main content -->
+<section>
+  <div class="container well">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <div id="mycarousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="item active"> <a href="services.html#plan"><img src="Optimized Images/planning_button.jpg" alt="Plan" class="img-responsive img-rounded"></a>
+              <h2 class="carousel-caption">Plan</h2>
+            </div>
+          </div>
+        </div>
+        <div id="plan" class="media">
+          <div class="media-body"> We all talk about planning but we don’t do it. Probably because we don't have a good framework to work with. Taking the time to consult and plan for growth will give you the steps you need to launch. </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <div id="mycarousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="item active"> <a href="services.html#build"><img src="Optimized Images/building_button.jpg" alt="Build" class="img-responsive img-rounded"></a>
+              <h2 class="carousel-caption">Build</h2>
+            </div>
+          </div>
+        </div>
+        <div class="media">
+          <div class="media-body"> Anyone can setup a basic website or social presence nowadays... but can you make it look good on mobile devices? Can you troubleshoot issues that arise when code is not working correctly? This is your reputation. </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <div id="mycarousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="item active"> <a href="services.html#report"><img src="Optimized Images/globalize_button.jpg" alt="Report" class="img-responsive img-rounded"></a>
+              <h2 class="carousel-caption">Flaunt</h2>
+            </div>
+          </div>
+        </div>
+        <div class="media">
+          <div class="media-body"> The key to a succesful online presence is working with what you got. What are the numbers you're working with? What kind of content can you show off? Mixing honest metrics with authentic style will take you places. </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="portfolio">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h2>Recent Work</h2>
+        <p>Here are some examples from recent projects</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3 text-center"> <img src="images/ne-sm-port.jpg" alt="" class="img-thumbnail img-responsive">
+        <h3>Mobile Responsive Website</h3>
+        <h6>NaturalEpicurean.com</h6>
+      </div>
+      <div class="col-md-3 text-center"> <img src="images/jmc-sm-port.jpg" alt="" class="img-thumbnail img-responsive">
+        <h3>SEO for Google</h3>
+        <h6>TrySomethingMore.com</h6>
+      </div>
+      <div class="col-md-3 text-center"> <img src="images/jh-sm-port.jpg" alt="" class="img-thumbnail img-responsive">
+        <h3>Wordpress Website</h3>
+        <h6>ferroelectronics.com</h6>
+      </div>
+      <div class="col-md-3 text-center"> <img src="images/fts-sm-port.jpg" alt="" class="img-thumbnail img-responsive">
+        <h3>Basic Website</h3>
+        <h6>FireTreeStudios.com</h6>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h4><a href="portfolio.html">View More</a></h4>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="sub_cta">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h2>We are your personal support to let you expand your wings and fly!</h2>
+        <p>For a free consultation about your online presence, don't hesitate to reach out and setup a time to talk.</p>
+		  <a href="contact.html"><button type="button" class="btn btn-info">Get in touch</button></a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div class="section well">
+    <div class="container">
+   	  <div class="row">
+		<div class="col-lg-4 col-md-4">
+            <h3 class="text-center">About</h3>
+            <h5>Super Heron Support is web-based company supporting small businesses with their online digital presence. They focus on helping clients set up and manage their websites, digital ads, online communities, mobile apps, and other online platforms.</h5>
+        </div>
+		<div class="col-lg-4 col-md-4">
+		  <h3 class="text-center">Contact</h3>
+          <address class="text-center">
+  <strong>Super Heron Support</strong><br>
+  info@superheron.com<br>
+  <abbr title="Phone">P:</abbr> 512.814.5533
+</address>
+</div>
+		<div class="col-lg-4 col-md-4">
+		  <h3 class="text-center">Facebook</h3>
+            <a href="http://facebook.com/superheron" target="_blank"><img class="center-block" src="images/FB-f-Logo__blue_512.png" class="img-responsive" width="50" height="50" alt="facebook icon"></a>
+		</div>
+</div>
+    </div>
+	</div>
+  <?php
+  include('includes/footer.php');
+  ?>
+	  
